ef3668c27735d9e54289bd79cd1e531fcac65b2e
carrieguo <593774767@qq.com>
2023-12-19 21:49:20 +0800

    编写与测试后端接口

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
+const express = require('express')
+const app = express()
 const data = [{
-    id:1,
+    id: 1,
     name: '华为mate60',
-    price:6900.00,
+    price: 6900.00,
     image: 'https://www.wanwupai.com/upload/product/20200502/1588400093veuFu6.jpg',
     intro: '卫星童话，五地面网络，仍可接受卫星电话',
-    max_order:1,
-    hot:true,
-    high:true
-},{
-    id:2,
+    max_order: 1,
+    hot: true,
+    high: true
+}, {
+    id: 2,
     name: '华为mate602',
-    price:6900.00,
+    price: 6900.00,
     image: 'https://www.wanwupai.com/upload/product/20200502/1588400093veuFu6.jpg',
     intro: '卫星童话，五地面网络，仍可接受卫星电话',
-    max_order:1,
-    hot:true,
-    high:true
+    max_order: 1,
+    hot: true,
+    high: true
 }]
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST')
+    next()
+})
+app.get('/products', (req, res) => {
+    res.json(data)
+})
+app.get('detail/:id', (req, res) => {
+    const {
+        id
+    } = req.params
+    const detail = data.find(item => item.id == id)
+    res.json(detail)
+})
+app.listen(3000)
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ RouterProvider 创建router实例，提供router配置
 */
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import router from "@/config/router";
+import { useEffect } from "react";
+import http from "@/utils/http";
+
+
+
 function App() {
+  /* 
+useEffect 宏任务
+useLayoutEffect 微任务
+事件环机制，微任务清空后GUI渲染，然后再出队列一个宏任务，再清空微任务队列，
+一般将请求放到宏任务中，loading事件放到微任务中，先渲染loding状态，再去做请求，避免白屏，对用户友好
+*/
+  useEffect(() => {
+    /* 回调函数不能写成asyc函数，否则会报错是因为 async函数返回的是一个promise， 而 useEffect函数默认要返回一个清除函数 
+    可以写成一个自制型函数来解决这个问题*/
+    ; (async () => {
+      const res = await http('/products')
+      console.log(res)
+    })()
+  }, [])
   return <RouterProvider router={createHashRouter(router)}></RouterProvider>;
 }
 
